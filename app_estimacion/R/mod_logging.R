28875718558c507d8382ca6cadf9a511fe680819
Stalyn Guerrero <guerrerostalyn@gmail.com>
2025-12-24 12:25:22 -0500

    Limpiando el proceso

--- app_estimacion/R/mod_logging.R
+++
@@ -1,9 +0,0 @@
-# mod_logging.R
-# Registro de eventos simple (JSON lines) en carpeta logs/
-log_event <- function(action, user = 'local', details = list()){ 
-  dir.create('logs', showWarnings = FALSE)
-  entry <- list(time = format(Sys.time(), tz = 'UTC'), user = user, action = action, details = details)
-  con <- file('logs/app_log.jsonl', open = 'a')
-  writeLines(jsonlite::toJSON(entry, auto_unbox = TRUE), con)
-  close(con)
-}
--- app_estimacion/app.r
+++
@@ -1,3 +0,0 @@
-source("ui.R")
-source('server.R')
-shinyApp(ui, server)
